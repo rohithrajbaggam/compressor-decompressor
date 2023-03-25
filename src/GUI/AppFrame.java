cac2aef9473d30b129674f53e5e17fdaa98b58a1
rohithcodebeat <rohith.baggam@codebeat.in>
2023-03-25 22:36:18 +0530

    Updated Color & Button Coordinates

--- src/GUI/AppFrame.java
+++ src/GUI/AppFrame.java
@@ -17,20 +17,34 @@ public class AppFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         compressButton = new JButton("Select file to compress");
-        compressButton.setBounds(20, 100, 200, 30);
+//        compressButton.setBounds(20, 100, 200, 30);
+        compressButton.setBounds(350, 100, 300, 30);
         compressButton.addActionListener(this);
         // this statement is used to get sources. like in actionPerformed we have ActionEvent e and statement create a source that helps to rise an action when it was called
 
 
         decompressButton = new JButton("Select file to decompress");
-        decompressButton.setBounds(250, 100, 200, 30);
+//        decompressButton.setBounds(20, 400, 200, 30);
+        decompressButton.setBounds(350, 200, 300, 30);
         decompressButton.addActionListener(this);
+
         // this statement is used to get sources. like in actionPerformed we have ActionEvent e and statement create a source that helps to rise an action when it was called
 
-        this.add(compressButton);
-        this.add(decompressButton);
+//        this.add(compressButton);
+//        this.add(decompressButton);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.add(compressButton);
+        buttonPanel.add(decompressButton);
+        buttonPanel.setBackground(Color.DARK_GRAY);
+
+        /*
+         a new JPanel is created with FlowLayout and the buttons are added to the panel. Then the panel is added to the content pane of the JFrame.
+         With this change, the compressButton and decompressButton should be arranged side-by-side in a row.
+         */
+        this.add(buttonPanel);
         this.setSize(1000, 500);
-        this.getContentPane().setBackground(Color.green);
+        this.setBackground(Color.BLACK);
+        this.getContentPane().setBackground(Color.BLACK);
         this.setVisible(true);
     }
 
